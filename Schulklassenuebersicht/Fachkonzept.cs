6ef8a6c0183371b7b7dd619e1554390f4c4914f3
fall1987 <pat.engel87@googlemail.com>
2017-11-16 11:55:41 +0100

    added Fachkonzept und IFachkonzept

--- Schulklassenuebersicht/Fachkonzept.cs
+++ Schulklassenuebersicht/Fachkonzept.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Fachkonzept
+namespace Schulklassenuebersicht
 {
-	
+    class Fachkonzept
+    {
+    }
 }
--- Schulklassenuebersicht/IFachkonzept.cs
+++ Schulklassenuebersicht/IFachkonzept.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public interface IFachkonzept
+namespace Schulklassenuebersicht
 {
-	
+    interface IFachkonzept
+    {
+    }
 }
